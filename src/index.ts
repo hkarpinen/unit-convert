7f12f2502501edd8a78528ae8ea53dd2c0a148a6
kuolintoive <karpinenh@gmail.com>
2022-01-29 17:29:57 -0700

    add imports/exports

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import { convertSimpleUnits, convertComplexUnits, convertSimpleUnitRate } from './convert/Unit';
-import { Unit, SimpleUnit, ComplexUnit, UnitKey, UnitRate } from './types/Unit';
+import { Unit, SimpleUnit, ComplexUnit, UnitKey, SimpleRate } from './types/Unit';
 import { PrefixAlias } from './types/Prefix';
 import { Dimension } from './types/Dimension';
 import { Dimensions } from './Dimensions';
+import { getPrefixShortNames } from './matcher/Prefix';
+import { getSingularUnitNames } from './matcher/Unit';
  
 module.exports = {
 	convertSimpleUnits: convertSimpleUnits,
 	convertComplexUnits: convertComplexUnits,
 	convertSimpleUnitRate: convertSimpleUnitRate,
-	dimensions: Dimensions
+	dimensions: Dimensions,
+	getPrefixShortNames: getPrefixShortNames,
+	getSingularUnitNames: getSingularUnitNames
 };
 
 export type {
@@ -16,7 +20,7 @@ export type {
 	SimpleUnit, 
 	ComplexUnit, 
 	UnitKey,
-	UnitRate,
+	SimpleRate,
 	PrefixAlias,
 	Dimension
 };
